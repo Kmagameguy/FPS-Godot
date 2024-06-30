fd23c3b61c6f46843c5c5ced10b990892ef7c4f3
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-06-29 22:57:19 -0400

    Add Jump Strength

--- controllers/scripts/player_jumping_state.gd
+++ controllers/scripts/player_jumping_state.gd
@@ -4,16 +4,30 @@ class_name PlayerJumpingState extends PlayerMovementState
 @export var ACCELERATION : float = 0.1
 @export var DECELERATION : float = 0.25
 @export var JUMP_VELOCITY: float = 4.5
+@export var DOUBLE_JUMP_VELOCITY : float = JUMP_VELOCITY
 @export_range(0.5, 1.0, 0.01) var INPUT_REDUCER: float = 0.85
+
+var DOUBLE_JUMP: bool = false
 
 func enter(_previous_state: PlayerState) -> void:
 	PLAYER.velocity.y += JUMP_VELOCITY
 	ANIMATION.pause()
+
+func exit() -> void:
+	DOUBLE_JUMP = false
 
 func update(delta: float) -> void:
 	PLAYER.update_gravity(delta)
 	PLAYER.update_input(SPEED * INPUT_REDUCER, ACCELERATION, DECELERATION)
 	PLAYER.update_velocity()
 	
+	if Input.is_action_just_pressed(PLAYER.STATES.JUMP.ACTION) && !DOUBLE_JUMP:
+		DOUBLE_JUMP = true
+		PLAYER.velocity.y = DOUBLE_JUMP_VELOCITY
+
+	if Input.is_action_just_released(PLAYER.STATES.JUMP.ACTION):
+		if PLAYER.velocity.y > 0:
+			PLAYER.velocity.y = PLAYER.velocity.y / 2.0
+
 	if PLAYER.is_on_floor():
 		transition.emit(PLAYER.STATES.IDLE.STATE_NAME)
