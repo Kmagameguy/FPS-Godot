b62af82ce39987f7c155ee10c9135d3034e8214f
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-07-07 17:11:52 -0400

    Apply Input Reducer to Falling State, Too

--- controllers/scripts/player_falling_state.gd
+++ controllers/scripts/player_falling_state.gd
@@ -4,6 +4,7 @@ class_name PlayerFallingState extends PlayerMovementState
 @export var ACCELERATION: float = 0.1
 @export var DECELERATION: float = 0.25
 @export var DOUBLE_JUMP_VELOCITY: float = 4.5
+@export_range(0.5, 1.0, 0.01) var INPUT_REDUCER: float = 0.85
 
 var _previous_state: PlayerState
 
@@ -13,7 +14,7 @@ func enter(previous_state: PlayerState) -> void:
 
 func update(delta: float) -> void:
 	PLAYER.update_gravity(delta)
-	PLAYER.update_input(SPEED, ACCELERATION, DECELERATION)
+	PLAYER.update_input(SPEED * INPUT_REDUCER, ACCELERATION, DECELERATION)
 	PLAYER.update_velocity()
 
 	if Input.is_action_just_pressed(PLAYER.STATES.JUMP.ACTION) && _not_previously_jumping():
